90aa4ffb3730409c93d500dc7a6dd7e1a76226ac
AlexandreBeraud <beraud.mailpro@gmail.com>
2023-10-25 15:01:10 +0200

    blblbl

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <router-views/>
-  </div>
+  <base-layout>
+    <router-view/>
+  </base-layout>
 </template>
+
+<script>
+import BaseLayout from '../src/views/BaseLayout.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BaseLayout
+  }
+}
+
+</script>
---
+++ src/views/BaseLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <HeaderView id="header"/>
+            <slot/>
+    </div>
+    
+</template>
+
+<script>
+import HeaderView from '../components/HeaderComponent.vue';
+
+export default {
+    name: 'BaseLayout',
+    components: {
+        HeaderView,
+    },
+ }
+</script>
+<style>
+</style>
